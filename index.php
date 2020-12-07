5e1571f8254d0409d6dca103907b23bd1feebbc1
shorty0207 <vytguc@gmail.com>
2020-12-07 22:46:01 +0200

    2 task

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+//$page = $_SERVER['PHP_SELF'];
+//$sec = "1";
+//header("Refresh: $sec; url=$page");
+?>
+<?php  date_default_timezone_set('Europe/Vilnius');?>
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -8,22 +14,16 @@ error_reporting(E_ALL);
 <!DOCTYPE html>
 <html>
 <head>
-    <title> PHP lydes<?php> 'As, ' . 'echo date('l \t\h\e jS')' . 'ir PHP' ?></title>
+    <meta charset="UTF-8">
+    <title> Aš valdau HTML!<?php print ' ir PHP' ;?></title>
 
 
-    <script src="https://kit.fontawesome.com/42762d49f1.js" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@600&display=swap" rel="stylesheet">
-
+   
 
 </head>
 <body>
-    
-    <div>
-        <img style= "width: 100px;"src="images/<?php print rand(1,6); ?>.png">
-    </div>
-
+        <h1><?php print 'Vytenis';?> -HTML <?php print' ir PHP' ;?> asas</h1>
+        <p>Kuriu dinamini turini.
+        Atidaryta: <?php print $today = date("F j, Y, g:i a") ?></p>
 </body>
 </html>
